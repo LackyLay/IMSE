df6203bfed3806073e8944087c36db531169ec36
Nikita <you@example.com>
2020-05-01 15:08:53 +0200

    123

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>>
+	<titel>Login Page</titel>
+
+</head>
+
+<body>
+		<form action = "Login/precess.php" method= "POST">
+
+				<label>Use Name:</label>
+				<input type= "text" name= "username" placeholder= "User Name" >
+
+			<button type = "submit"> Login</button>
+			
+
+		</form>
+	
+
+
+</body>
+
+
+
+
+
+
+</html>
