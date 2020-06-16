cbdc2d6d52ece02d9350a0ef803a25eb7d1eb129
Nikita <you@example.com>
2020-06-16 02:08:07 +0200

    new

--- mongodb/src/main/java/mongoMain.java
+++ mongodb/src/main/java/mongoMain.java
@@ -27,9 +27,7 @@ public class mongoMain {
 
             Statement stmt=con.createStatement();
             ResultSet rs=stmt.executeQuery("select * from member");
-
-
-
+            
             try (var mongoClient = MongoClients.create("mongodb://localhost:27017")) {
 
                 var database = mongoClient.getDatabase("wineapp");
@@ -51,9 +49,10 @@ public class mongoMain {
                     Statement stmt3=con.createStatement();
                     ResultSet rs3=stmt3.executeQuery("select * from want_try where member_id like " + rs.getString("mem_id"));
 
+
                     MongoCollection<Document> collection = database.getCollection("member");
 
-                    var d1 = new Document("_id", rs.getInt("mem_id"));
+                    var d1 = new Document("_id", rs.getString("mem_id"));
                     d1.append("nickname", rs.getString("nickname"));
                     d1.append("country", rs.getString("country"));
                     d1.append("status", rs.getString("status"));
@@ -61,18 +60,48 @@ public class mongoMain {
                     List list = new ArrayList();
 
 
-                    while(rs2.next()) {
-                        list.add(rs2.getString("member_id2"));
+//------------------NEW-----------------------------------------
+                    List listfollow = new ArrayList();
+
+                        while (rs2.next()) {
+                            String strBuff = rs2.getString("member_id2");
+
+                            Statement followStmt=con.createStatement();
+                            ResultSet followRes=followStmt.executeQuery("select * from member where mem_id like " + strBuff);
+
+                            while (followRes.next()) {
+
+                                var d2 = new Document("_id", followRes.getString("mem_id"));
+                                d2.append("nickname", followRes.getString("nickname"));
+                                d2.append("country", followRes.getString("country"));
+                                d2.append("status", followRes.getString("status"));
+
+                                listfollow.add(d2);
+                            }
+                        }
+                        d1.append("follow", listfollow);
+
+                    List listTry = new ArrayList();
+
+                    while (rs3.next()) {
+                        String strBuff = rs3.getString("wine_id");
+
+                        Statement tryStmt=con.createStatement();
+                        ResultSet tryRes=tryStmt.executeQuery("select * from wine where wine_id like " + strBuff);
+
+                        while (tryRes.next()) {
+
+                            var d3 = new Document("_id", tryRes.getString("wine_id"));
+                            d3.append("color", tryRes.getString("color"));
+                            d3.append("vintage", tryRes.getString("vintage"));
+                            d3.append("winery_name", tryRes.getString("winery_name"));
+                            d3.append("grape_name", tryRes.getString("grape_name"));
+
+                            listTry.add(d3);
+                        }
                     }
-                    d1.append("follow", list);
-
-                    List list2 = new ArrayList();
-
-
-                    while(rs3.next()) {
-                        list2.add(rs3.getString("wine_id"));
-                    }
-                    d1.append("want_try", list2);
+                    d1.append("want_try", listTry);
+//------------------END NEW-----------------------------------------
                     collection.insertOne(d1);
                 }
 
@@ -92,9 +121,6 @@ public class mongoMain {
                     Statement stmtReview=con.createStatement();
                     ResultSet rsReview=stmtReview.executeQuery("select * from review where wine_id like " + rsWine.getString("wine_id"));
 
-
-
-
                     MongoCollection<Document> collection2 = database.getCollection("wine");
 
                     var d1 = new Document("_id", rsWine.getInt("wine_id"));
@@ -106,9 +132,6 @@ public class mongoMain {
 
                     List listReview = new ArrayList();
 
-
-
-
                     while(rsReview.next()) {
                         var d2 = new Document("_id", rsReview.getInt("review_id"));
                         d2.append("points", rsReview.getString("points"));
@@ -119,26 +142,32 @@ public class mongoMain {
                     }
                     d1.append("review", listReview);
 
-                    List listGrape = new ArrayList();
-
-
-
-
-
-
-
-
-
-
-
-
                     collection2.insertOne(d1);
                 }
 
+//------------------Grape------------------------------------- NEW
+                Statement grapeStmt=con.createStatement();
+                ResultSet grapeWine=wineStmt.executeQuery("select * from grape");
+
+                try {
+                    database.createCollection("grape");
+                    System.out.println("Collection created successfully");
+                } catch (MongoCommandException e) {
+                    database.getCollection("grape").drop();
+                }
+
+                while(grapeWine.next()) {
+
+                    MongoCollection<Document> collectionGrape = database.getCollection("grape");
+
+                    var d1 = new Document("_id", grapeWine.getString("grape_name"));
+                    d1.append("food_paring", grapeWine.getString("food_paring"));
+                    d1.append("acidity", grapeWine.getString("acidity"));
 
 
-
-
+                    collectionGrape.insertOne(d1);
+                }
+                //------END NEW
 
                 MongoCollection < Document > collection = database.getCollection("member");
                 try (MongoCursor< Document > cur = collection.find().iterator()) {
